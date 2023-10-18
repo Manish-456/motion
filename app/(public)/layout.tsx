71b5f472682079a87e3b178c2f14047f851c306e
Manish Tamang <111108242+Manish-456@users.noreply.github.com>
2023-10-18 15:38:23 +0545

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PublicLayout({children} : {children : React.ReactNode}) {
   return (
-    <div className='h-full bg-[#1F1F1F]'>
+    <div className='min-h-screen bg-[#1F1F1F]'>
       {children}
     </div>
   )
